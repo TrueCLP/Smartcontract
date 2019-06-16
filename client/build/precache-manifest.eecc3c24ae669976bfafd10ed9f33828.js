499eff9c587c0c8360bb8f646f21e560fdca65fe
Vulturio <cryptowea@gmail.com>
2019-06-15 22:19:50 -0400

    fix site2

---
+++ client/build/precache-manifest.eecc3c24ae669976bfafd10ed9f33828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3866bbb6d985b0752370",
+    "url": "/TrueCLP/Smartcontract/client/build/static/css/main.b9babdc0.chunk.css"
+  },
+  {
+    "revision": "3866bbb6d985b0752370",
+    "url": "/TrueCLP/Smartcontract/client/build/static/js/main.3866bbb6.chunk.js"
+  },
+  {
+    "revision": "b3881846992ab96fb8ca",
+    "url": "/TrueCLP/Smartcontract/client/build/static/js/1.b3881846.chunk.js"
+  },
+  {
+    "revision": "29b385d194879912452c",
+    "url": "/TrueCLP/Smartcontract/client/build/static/js/runtime~main.29b385d1.js"
+  },
+  {
+    "revision": "6f18ec08cc3ac6805bacd9227e18ba0f",
+    "url": "/TrueCLP/Smartcontract/client/build/index.html"
+  }
+];
