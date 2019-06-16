daebada138e65e7fdbd14815e6d64e98ca8e16af
Vulturio <cryptowea@gmail.com>
2019-06-15 22:23:41 -0400

    Updates

---
+++ precache-manifest.32bbfee09ee0f554bbba1f2a465a7004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2b78b85c32dd2230a68",
+    "url": "/Smartcontract/static/css/main.b9babdc0.chunk.css"
+  },
+  {
+    "revision": "c2b78b85c32dd2230a68",
+    "url": "/Smartcontract/static/js/main.c2b78b85.chunk.js"
+  },
+  {
+    "revision": "b3881846992ab96fb8ca",
+    "url": "/Smartcontract/static/js/1.b3881846.chunk.js"
+  },
+  {
+    "revision": "ea1034a1020565648d62",
+    "url": "/Smartcontract/static/js/runtime~main.ea1034a1.js"
+  },
+  {
+    "revision": "ffcb3b9a286c1c2eaebbdf94df48284d",
+    "url": "/Smartcontract/index.html"
+  }
+];
